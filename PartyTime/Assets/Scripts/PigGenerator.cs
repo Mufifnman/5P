a2c3c228cf4d7a2cb51b044ba325373aec22fe66
Forrest Keppler <kelpycreek@gmail.com>
2017-12-01 22:52:12 -0800

    updates

---
+++ PartyTime/Assets/Scripts/PigGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigGenerator : MonoBehaviour
+{
+    public GameObject PigPrefab;
+    private GameObject currentPig;
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentPig = Instantiate<GameObject>(PigPrefab);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnClicked()
+    {
+        //aSSIGn CURRENT PIG TO Hands 
+        // InteracitonManger.Instance.clickedHand.SetObject(currentPig);
+
+        currentPig = Instantiate<GameObject>(PigPrefab);
+    }
+}
