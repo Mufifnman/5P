58e88304100511487142ecdb23a728bc4093895c
Kevin Luecke <kjluecke@gmail.com>
2017-12-03 13:04:32 -0800

    working on timer

---
+++ PartyTime/Assets/updatetext.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class updatetext : MonoBehaviour {
+
+    public Text text;
+    public Something script;
+	// Use this for initialization
+	void Start () {
+        script = transform.getParent.getComponent("cycle");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.Text = script.text;
+
+	}
+}
